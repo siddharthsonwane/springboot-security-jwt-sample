a2c20c959dd60f8d1ed2696eb703eee248c02ecf
siddharth.sonwane <siddharthsonwane18@gmail.com>
2023-12-22 12:59:18 +0530

    sample method added

--- src/main/java/com/example/SpringbootSecurityJwtSampleApplication.java
+++ src/main/java/com/example/SpringbootSecurityJwtSampleApplication.java
@@ -2,12 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootSecurityJwtSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootSecurityJwtSampleApplication.class, args);
+	}
+	
+	@GetMapping("/welcome")
+	public String greting() {
+		return "welcome to Spring Boot Security Sample";
 	}
 
 }
